a6eb878b8d6dac5f7a95909a5ea1fb49c8ab30f0
Jon Langlois <jonathan-langlois@live.ca>
2020-06-15 14:59:09 -0700

    add required and unique fields

--- src/db/post.ts
+++ src/db/post.ts
@@ -1,13 +1,12 @@
 import mongoose, { Schema } from 'mongoose';
 
 const PostSchema = new mongoose.Schema({
-  _id: Schema.Types.ObjectId,
   upvotes: [{type: Schema.Types.ObjectId, ref: 'User', time: {type: Date, default: Date.now}}],
   downvotes: [{type: Schema.Types.ObjectId, ref: 'User'}],
   tags: [{type: Schema.Types.ObjectId, ref: 'Tag'}],
-  address: String,
-  latitude: String,
-  longitude: String
+  address: {type: String, required: true},
+  latitude: {type: String, required: true},
+  longitude: {type: String, required: true}
 });
 
 module.exports = PostSchema;
