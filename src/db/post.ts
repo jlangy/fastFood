19aa537cc6085c5778405122355b56bb2fb7e410
Jon Langlois <jonathan-langlois@live.ca>
2020-06-15 15:01:35 -0700

    validate at least one tag on a post

--- src/db/post.ts
+++ src/db/post.ts
@@ -1,9 +1,13 @@
 import mongoose, { Schema } from 'mongoose';
 
+function atLeastOne(val: string[]){
+  return val.length > 0;
+}
+
 const PostSchema = new mongoose.Schema({
   upvotes: [{type: Schema.Types.ObjectId, ref: 'User', time: {type: Date, default: Date.now}}],
   downvotes: [{type: Schema.Types.ObjectId, ref: 'User'}],
-  tags: [{type: Schema.Types.ObjectId, ref: 'Tag'}],
+  tags: {type: [{type: Schema.Types.ObjectId, ref: 'Tag'}], validate: atLeastOne},
   address: {type: String, required: true},
   latitude: {type: String, required: true},
   longitude: {type: String, required: true}
