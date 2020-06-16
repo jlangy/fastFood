ebd2622be848e2f16c4a4c4ea14e8fa6ab87dbb1
Jon Langlois <jonathan-langlois@live.ca>
2020-06-16 11:30:13 -0700

    get posts/:id route

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,22 +1,26 @@
 import express = require('express');
 import mongoose from 'mongoose';
 import PostSchema from '../db/post';
+import TagSchema from '../db/tag';
 import { getTagIds } from '../helpers';
 const posts = express.Router();
 
-posts.get('/:id', (req,res) => {
-  //TODO: create new post
-  res.send('got /posts/:id')
+posts.get('/:id', async (req,res) => {
+  const id = req.params.id;
+  const Post = mongoose.model('Post', PostSchema);
+  const Tag = mongoose.model("Tag", TagSchema)
+  const post = await Post.findOne({_id: id}).populate('tags');
+  res.json(post)
 })
 
 posts.get('/', (req, res) => {
-  //TODO: return relevant posts
+  console.log(req.body.params)
   res.send('got /posts')
 });
 
 posts.post('/', async (req,res) => {
   //TODO: create new post
-  const Post = mongoose.model('User', PostSchema);
+  const Post = mongoose.model('Post', PostSchema);
   const tags = await getTagIds(req.body);
   const post = new Post({...req.body, tags});
   post.save((err, user) => {
