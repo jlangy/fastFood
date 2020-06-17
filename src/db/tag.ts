b2b8f9da204e51e088cfcfaccc80eca24617d476
Jon Langlois <jonathan-langlois@live.ca>
2020-06-17 09:52:39 -0700

    Posts routes
    What:
    
    1) Get /posts
    2) Get /posts/:id
    3) post /posts
    4) Removed tag schema

--- src/db/tag.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const TagSchema = new mongoose.Schema({
-  name: {type: String, unique: true}
-});
-
-export default TagSchema;
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,34 +1,4 @@
-import TagSchema from './db/tag';
 import mongoose from 'mongoose';
-
-interface Post {
-  tags: string[],
-  address: string,
-  latitude: string,
-  longitude: string
-}
-
-//Retrieve ref id's, create tag if it doesn't exist 
-export function getTagIds(post: Post){
-  const tags: string[] = [];
-  const Tag = mongoose.model('Tag', TagSchema);
-  return new Promise(async (resolve, reject) => {
-    for(let tag of post.tags){
-      const tagExists = await Tag.findOne({name: tag})
-      if(!tagExists){
-        //Create new tag if doesnt exist
-        const newTag = new Tag({name: tag})
-        await newTag.save((err, tag) => {
-          if(err) reject('saving tag failed');
-          else tags.push(tag._id);
-        });
-      } else {
-        tags.push(tagExists._id)
-      }
-    }
-    resolve(tags)
-  })
-}
 
 export function convertLatLongToDistance(lat1: number, lat2: number, long1: number, long2: number){
   const R = 6371e3;
