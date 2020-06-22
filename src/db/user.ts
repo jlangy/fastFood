abc324dd521f6496a368caf63be25be807d8e92a
Jon Langlois <jonathan-langlois@live.ca>
2020-06-22 12:22:06 -0700

    set user id

--- src/db/user.ts
+++ src/db/user.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
 const UserSchema = new mongoose.Schema({
+  _id: String,
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
