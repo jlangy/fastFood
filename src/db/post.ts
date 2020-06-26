5020bdee6244ccf1df2dd84b8993e587b47aaf35
Harrish Suhumar <harrish.s@hotmail.ca>
2020-06-25 22:53:41 -0400

    add imgurl entry to post schema

--- src/db/post.ts
+++ src/db/post.ts
@@ -1,24 +1,35 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-function atLeastOne(val: string[]){
+function atLeastOne(val: string[]) {
   return val.length > 0;
 }
 
 const PostSchema = new mongoose.Schema({
   _id: String,
+  imageUrl: { type: String, required: true },
   // upvotes: [{User: {type: Schema.Types.ObjectId, ref: 'User'}, time: {type: Date, default: Date.now}}],
   //For seed data, remove for prod
-  upvotes: [{User: {type: String, ref: 'User'}, time: {type: Date, default: Date.now}}],
+  upvotes: [
+    {
+      User: { type: String, ref: "User" },
+      time: { type: Date, default: Date.now },
+    },
+  ],
   //For seed data, remove for prod
-  downvotes: [{User: {type: String, ref: 'User'}, time: {type: Date, default: Date.now}}],
+  downvotes: [
+    {
+      User: { type: String, ref: "User" },
+      time: { type: Date, default: Date.now },
+    },
+  ],
   // downvotes: [{User: {type: Schema.Types.ObjectId, ref: 'User'}, time: {type: Date, default: Date.now}}],
-  tags: {type: [String], validate: atLeastOne},
-  address: {type: String, required: true},
-  storename: {type: String, required: true},
-  location: { type: [Number], index: '2d'},
-  price: { type: Number, required: true},
-  discountPrice: { type: Number, required: true},
-  createdAt: {type: Date, default: Date.now},
+  tags: { type: [String], validate: atLeastOne },
+  address: { type: String, required: true },
+  storename: { type: String, required: true },
+  location: { type: [Number], index: "2d" },
+  price: { type: Number, required: true },
+  discountPrice: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default PostSchema;
